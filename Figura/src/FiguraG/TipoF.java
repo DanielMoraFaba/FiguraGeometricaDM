12db25a6e33ec849695d3aa162261b4cb14c0b53
moraf <morafabadaniel@gmail.com>
2022-03-03 18:04:12 +0100

    Documentado

--- Figura/src/FiguraG/TipoF.java
+++ Figura/src/FiguraG/TipoF.java
@@ -2,15 +2,15 @@ package FiguraG;
 
 public class TipoF {
 
-	public static void TipoF(int base, int altura) {
-		String figura;
-		if (base == altura) {
-			figura = "cuadrado";
-		} else {
-			figura = "rectángulo";
-		}
-		int perimetro = 2 * base + 2 * altura;
-		System.out.println("La figura es un " + figura + " y su perímetro es: " + perimetro);
-	}
+	public static void TipoF(int base, int altura) { 											// I
+		String figura;                               											// 
+		if (base == altura) {                        											// 1 
+			figura = "cuadrado";                     											// 2
+		} else {                                     											// 
+			figura = "rectángulo";                 											    // 3
+		}																						//
+		int perimetro = 2 * base + 2 * altura;                                                  //
+		System.out.println("La figura es un " + figura + " y su perímetro es: " + perimetro);   // 4
+	}                                                                                           //
 
 }
